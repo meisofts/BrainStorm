24525a30d3203a0a030392b7803c24c7fd5f778e
Martins  Olawale Williams <martins.eguaoje@gmail.com>
2025-07-13 01:05:36 +0100

    Main

--- app/config.py
+++ app/config.py
@@ -4,10 +4,3 @@ class Config:
     # SECRET_KEY = os.getenv('SECRET_KEY', 'your_key')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://yourDBName:yourDBPass@localhost:5432/yourDB')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-<<<<<<< HEAD
-#.env SECRET_KEY="your_key"  ==> Key for .env file
-=======
-#.env SECRET_KEY="you_key"  ==> Key for .env file
->>>>>>> 5fb4167a4de3fb9fa01850184040d4c1cbcae939
